85ad5d75dbe3b64c812f00a392e22577a6a12bc7
JordanWitt <wittjordan94@gmail.com>
2022-12-05 15:09:02 -0600

    sout testing in console

--- src/test/javatest/StudentTest.java
+++ src/test/javatest/StudentTest.java
@@ -16,22 +16,27 @@ public class StudentTest {
         studentWithManyGrades.addGrade(90);
         studentWithManyGrades.addGrade(80);
         studentWithManyGrades.addGrade(50);
+        System.out.println(jordan.getName());
+        System.out.println(studentWithManyGrades.getName() + studentWithManyGrades.getGrades());
     }
 
     @Test
     public void ifUserObjectCanBeCreated(){
         assertEquals("Jordan Witt", jordan.getName());
         assertEquals(1, jordan.getId());
+        System.out.println(jordan.getId());
     }
 
     @Test
     public void testIfAddGradeWorks(){
         assertSame(90, jordan.getGrades().get(0));
+        System.out.println(jordan.grades);
     }
 
     @Test
     public void testIfAvgIsCorrect(){
         assertEquals(90, jordan.getGradeAverage(), 0);
         assertEquals(73.33333333333333, studentWithManyGrades.getGradeAverage(), 0);
+        System.out.println(studentWithManyGrades.getName() + " " + studentWithManyGrades.getGradeAverage() + " " + jordan.getName() + jordan.getGradeAverage());
     }
 }
